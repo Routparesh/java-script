1063cd607b5228bace76ba29af6183f8a016c41c
Paresh Kumar Rout Kumar <101355021+Routparesh@users.noreply.github.com>
2022-09-22 08:47:28 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+/*problem -1
+let a = prompt("Enter your age");
+if(a > 10 && a<20){
+  console.log("This is  valid age")
+}
+else{
+  console.log("This is invalid age")
+}
+*/ 
+/*
+
+problem-2 switch case
+const age = prompt("enter your age");
+switch (age) {
+  case '12':
+    console.log("This is 12")
+    break;
+  case '13':
+    console.log("This is 13")
+    break;
+  case '14':
+    console.log("this is 14")
+    break;
+  case '15':
+    console.log("this is 15")
+    break;
+  default:
+    console.log("This is not a number")
+}
+*/
+
+/*
+problem-3
+let num = prompt("Enter a number");
+if(num%2==0 && num%3==0){
+  console.log("Number divided 2 & 3")
+}
+else{
+  console.log("Number not divided 2 & 3")
+}
+*/
+
+/*
+problem-4 (||)or function
+let num = prompt("Enter a number");
+if(num%2==0 || num%3==0){
+  console.log("True")
+}
+else{
+  console.log("False")
+}
+  */
+
+
+//problem-5 Turnary operator
+let age = prompt("Enter your age")
+let b= age>18? "you can drive" : "you cant drive" 
+console.log(b)
+
